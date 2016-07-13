01f613e9b6da0eeab6e547d7f269869c95281ec1
Yana Kultysheva <ykultysheva@gmail.com>
2016-07-13 16:49:37 -0400

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Yana Kultysheva on 2016-07-13.
+//  Copyright © 2016 Yana Kultysheva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Yana Kultysheva on 2016-07-13.
+//  Copyright © 2016 Yana Kultysheva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
